2668c1b105fd07ebb844adfba55262bcb6aa20b2
Artur_Kazlouski <Artur_Kazlouski@epam.com>
2018-02-20 17:51:12 +0300

    fixed gap

--- passport/index.js
+++ passport/index.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../mongoose/models/user');
 
 passport.use(
   new LocalStrategy(
