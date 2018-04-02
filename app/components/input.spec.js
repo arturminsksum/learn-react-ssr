9337bb293aef3c3b32db3273665a2ac5e8bfbe2e
Artur Minsk <artur.minsk.sum@gmail.com>
2018-04-02 23:42:04 +0300

    input test

---
+++ app/components/input.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Input from './input';
+
+describe('Input', () => {
+  const wrapper = shallow(
+    <Input type="password" label="Password" name="password" />,
+  );
+  const label = wrapper.find('label');
+  const input = wrapper.find('input');
+  console.log(wrapper.debug());
+
+  it('should render label', () => {
+    expect(label.text()).toEqual('Password');
+  });
+
+  it('should render Input type password', () => {
+    expect(input.prop('type')).toEqual('password');
+  });
+
+  it('should render Input name', () => {
+    expect(input.prop('name')).toEqual('password');
+  });
+});
